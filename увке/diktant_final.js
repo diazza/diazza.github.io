9d8969d2b861de2055957bb1184fb232c233a74c
diazza <64833015+diazza@users.noreply.github.com>
2021-11-26 17:04:28 +0600

    Add files via upload

---
+++ увке/diktant_final.js
@@ -0,0 +1,57 @@
+// // var script = document.createElement('script');
+// // script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+// // script.type = 'text/javascript';
+// // document.getElementsByTagName('head')[0].appendChild(script);
+
+// /*1*/let a = {};
+
+// /*2*/let b = [1, 2, 3]
+
+// /*3*/function c(a,b){
+//     if(a > b){
+//     return a
+//     }else {
+//         return b
+//     }
+// }
+// /*4*/console.log(b[2])
+
+// /*5*/b.push("4");
+// // alert(b);
+
+// /*6*/console.log(b[b.lenght -1])
+
+// /*7*/const shyndyk = true;
+// /*8*/if(shyndyk){
+//     console.log('shyndyk')
+// }else{
+//     console.log('otyrik')
+// }
+
+// /*9*/let go = ['Serik','Kaisar','Dias','Danyar','Janer']
+// /*10*/function search(users){
+//     go.forEach(af => {
+//         if (af == users){
+//             console.log(users);
+//         }else{
+//             console.log('kosyak')
+//         }
+//     })
+// }
+// search('Serik')
+
+/*11*/let dream = 'я хочу кушать  '
+ /*12*/ dream = dream.split(" ").join("")
+ console.log(dream.length)
+
+ 
+
+  /*13*/ 
+// /*14*/ let center = [15,15]
+// let number = 10
+// let number1 = 10/2
+// let a = [center[0]-number1,center[1]+number1]
+// let b = [center[0]+number1,center[1]+number1]
+// let c = [center[0]+number1,center[1]-number1]
+// let d = [center[0]-number1,center[1]-number1]
+// console.log(a,b,c,d)
